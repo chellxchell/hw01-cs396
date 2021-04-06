2467ebdafb3e353921eef2f20476a99546a46792
chellxchell <rochellecompendio2021@u.northwestern.edu>
2021-04-05 20:16:36 -0500

    finished get methods

--- src/routes.js
+++ src/routes.js
@@ -20,10 +20,57 @@ router.route("/")
 
 const data = require("../config/data.json");
 
+// returns doctor object given their ID
+function getDoctor(id) {
+    // look for doctor with correct id
+    for (var doctor of data.doctors) {
+        if (doctor._id == id) {
+            return doctor
+        }
+    }
+    // if one isn't found
+    return null
+}
+// returns companion object given ID
+function getCompanion(id) {
+    // look for companion with correct id
+    for (var companion of data.companions) {
+        if (companion._id == id) {
+            return companion
+        }
+    }
+    // if one isn't found
+    return null
+}
+
+// returns companions for doctor given id
+function getDocCompanions(docID) {
+    var companionList = []
+    // loop through each companion
+    for (var companion of data.companions) {
+        // check if companion had that doctor
+        if (companion.doctors.includes(docID)) {
+            companionList.push(companion)
+        }
+    }
+    return companionList
+}
+// returns doctors for companion given id
+function getCompDoctors(compID) {
+    var docList = []
+    var companion = getCompanion(compID)
+    // loop through each doctor
+    for (var docID of companion.doctors) {
+        let doc = getDoctor(docID)
+        docList.push(doc)
+    }
+    return docList
+}
+
 router.route("/doctors")
     .get((req, res) => {
         console.log("GET /doctors");
-        res.status(501).send();
+        res.status(200).send(data.doctors);
     })
     .post((req, res) => {
         console.log("POST /doctors");
@@ -33,7 +80,18 @@ router.route("/doctors")
 router.route("/doctors/:id")
     .get((req, res) => {
         console.log(`GET /doctors/${req.params.id}`);
-        res.status(501).send();
+
+        var doctor = getDoctor(req.params.id)
+
+        if (doctor) {
+            res.status(200).send(doctor);
+        }
+        else {
+            res.status(404).send({
+                message: `Doctor with id ${req.params.id} does not exist.`
+            });
+        }
+
     })
     .patch((req, res) => {
         console.log(`PATCH /doctors/${req.params.id}`);
@@ -47,7 +105,17 @@ router.route("/doctors/:id")
 router.route("/doctors/:id/companions")
     .get((req, res) => {
         console.log(`GET /doctors/${req.params.id}/companions`);
-        res.status(501).send();
+
+        // check if doctor exists
+        var doctor = getDoctor(req.params.id)
+        if (!doctor) {
+            res.status(404).send({
+                message: `Doctor with id ${req.params.id} does not exist.`
+            });
+        }
+
+        var companionList = getDocCompanions(req.params.id)
+        res.status(200).send(companionList);
     });
 
 // router.route("/doctors/:id/companions/longest")
@@ -59,13 +127,31 @@ router.route("/doctors/:id/companions")
 router.route("/doctors/:id/goodparent")
     .get((req, res) => {
         console.log("GET /doctors/:id/goodparent");
-        res.status(501).send();
+
+        // check if doctor exists
+        var doctor = getDoctor(req.params.id)
+        if (!doctor) {
+            res.status(404).send({
+                message: `Doctor with id ${req.params.id} does not exist.`
+            });
+        }
+
+        // get list of doc's companions
+        var companionList = getDocCompanions(req.params.id)
+        for (var companion of companionList) {
+            // if one companion not alive
+            if (!companion.alive) {
+                res.status(200).send(false);
+            }
+        }
+
+        res.status(200).send(true);
     });
 
 router.route("/companions")
     .get((req, res) => {
         console.log("GET /companions");
-        res.status(501).send();
+        res.status(200).send(data.companions);
     })
     .post((req, res) => {
         console.log("POST /companions");
@@ -75,13 +161,32 @@ router.route("/companions")
 router.route("/companions/crossover")
     .get((req, res) => {
         console.log(`GET /companions/crossover`);
-        res.status(501).send();
+
+        var companionList = []
+        for (var companion of data.companions){
+            // if they travelled with > 1 doctor
+            if (getCompDoctors(companion._id).length > 1){
+                companionList.push(companion)
+            }
+        }
+        res.status(200).send(companionList);
+
     });
 
 router.route("/companions/:id")
     .get((req, res) => {
         console.log(`GET /companions/${req.params.id}`);
-        res.status(501).send();
+
+        var companion = getCompanion(req.params.id)
+
+        if (companion) {
+            res.status(200).send(companion);
+        }
+        else {
+            res.status(404).send({
+                message: `Companion with id ${req.params.id} does not exist.`
+            });
+        }
     })
     .patch((req, res) => {
         console.log(`PATCH /companions/${req.params.id}`);
@@ -95,13 +200,45 @@ router.route("/companions/:id")
 router.route("/companions/:id/doctors")
     .get((req, res) => {
         console.log(`GET /companions/${req.params.id}/doctors`);
-        res.status(501).send();
+
+        // check if companion exists
+        var companion = getCompanion(req.params.id)
+        if (!companion) {
+            res.status(404).send({
+                message: `Companion with id ${req.params.id} does not exist.`
+            });
+        }
+
+        var docList = getCompDoctors(req.params.id)
+        res.status(200).send(docList);
+
     });
 
 router.route("/companions/:id/friends")
     .get((req, res) => {
         console.log(`GET /companions/${req.params.id}/friends`);
-        res.status(501).send();
+
+        // check if companion exists
+        var companion = getCompanion(req.params.id)
+        if (!companion) {
+            res.status(404).send({
+                message: `Companion with id ${req.params.id} does not exist.`
+            });
+        }
+
+        var friendList = []
+        // go through each companion
+        for (var friend of data.companions) {
+            // make sure it's not the same companion
+            if (friend._id == req.params.id) {
+                continue
+            }
+            // if they share at least one season
+            if (companion.seasons.some(r => friend.seasons.includes(r))) {
+                friendList.push(friend)
+            }
+        }
+        res.status(200).send(friendList);
     });
 
 //////////////////
